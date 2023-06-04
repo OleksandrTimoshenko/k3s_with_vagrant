ed8d79f50cf2506865785fcbe0cf258d0ddf59d0
Oleksandr <o.tymoshenko@astoundcommerce.com>
2023-06-04 23:34:16 +0300

    add default endpoint

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import health_check_view
+from .views import health_check_view, index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('health/', health_check_view, name='health_check'),
 ]
--- django/mysite/mysite/views.py
+++ django/mysite/mysite/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 def health_check_view(request):
     # You can perform additional health check logic here if needed
     return HttpResponse("OK")
+
+def index(request):
+    return HttpResponse("Hello, this is the default endpoint!")
